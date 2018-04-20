f9cf11ddba01bef2b7d61864fd9c7b4aca69c5b5
alumno <alumno>
2018-04-19 21:25:41 -0300

    facultad

---
+++ Ejercicio31/SistemaDeAtencion/Cliente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaDeAtencion;
+
+namespace SistemaDeAtencion
+{
+    public class Cliente
+    {
+        #region Fields
+        private string nombre;
+        private int numero;
+        #endregion
+
+        #region Properties
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public int Numero
+        {
+            get
+            {
+                return this.numero;
+            }
+            
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Cliente(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public Cliente(int numero, string nombre)
+            :this(numero)
+        {
+            this.Nombre = nombre;
+        }
+        
+        public static bool operator !=(Cliente c1, Cliente c2)
+        {
+            bool retorno = false;
+            if(object.ReferenceEquals(c1.Numero,c2.Numero))
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public static bool operator ==(Cliente c1, Cliente c2)
+        {
+            bool retorno = true;
+            if (c1!=c2)
+            {
+                retorno = false;
+            }
+            return retorno;
+        }
+        #endregion
+
+    }
+}
---
+++ Ejercicio31/SistemaDeAtencion/PuestoAtencion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SistemaDeAtencion
+{
+    class PuestoAtencion
+    {
+        #region Fields
+        private static int numeroAcutal;
+        private Puesto puesto;
+
+        #endregion
+
+        #region properties
+
+        public int NumeroActual
+        {
+            get
+            {
+                return numeroAcutal;
+            }
+        }
+
+        #endregion
+
+        #region methods
+        public bool Atender(Cliente cli)
+        {
+            bool retorno =  false;
+            Thread.Sleep(5000);
+            retorno = true;
+            return retorno;
+        }
+
+        private static PuestoAtencion()
+        {
+            numeroAcutal = 0;
+        }
+
+        public PuestoAtencion(Puesto puesto)
+        {
+            this.puesto = puesto;
+        }
+
+        #endregion
+        
+        #region Nested Types
+
+        public enum Puesto
+        {
+            caja1,
+            caja2,
+        }
+
+        #endregion
+    }
+}
