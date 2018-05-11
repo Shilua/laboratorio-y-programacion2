2647e2717f1925b0db06d57d6280ce1250a1ca29
shilua <shilua4@gmail.com>
2018-05-11 08:34:10 -0300

    parcial

---
+++ parcial1/VistaForm/VistaForm.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace VistaForm
+{
+    partial class VistaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbDatosOficina = new System.Windows.Forms.GroupBox();
+            this.lblPisoOficina = new System.Windows.Forms.Label();
+            this.lblDepartamentoOficina = new System.Windows.Forms.Label();
+            this.lblNombreJefe = new System.Windows.Forms.Label();
+            this.lblApellidoJefe = new System.Windows.Forms.Label();
+            this.Ingreso = new System.Windows.Forms.Label();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.btnCrearOficina = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.grbDatosEmpleado = new System.Windows.Forms.GroupBox();
+            this.nudPisoOficina = new System.Windows.Forms.NumericUpDown();
+            this.cmbDepartamentoOficina = new System.Windows.Forms.ComboBox();
+            this.txtNombreJefe = new System.Windows.Forms.TextBox();
+            this.txtApellidoJefe = new System.Windows.Forms.TextBox();
+            this.txtDocumentoJefe = new System.Windows.Forms.TextBox();
+            this.dtpFechaIngreso = new System.Windows.Forms.DateTimePicker();
+            this.rtbDatos = new System.Windows.Forms.RichTextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblLegajo = new System.Windows.Forms.Label();
+            this.lblPiso = new System.Windows.Forms.Label();
+            this.lblDepartamento = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtLegajo = new System.Windows.Forms.TextBox();
+            this.nudPiso = new System.Windows.Forms.NumericUpDown();
+            this.cmbDepartamento = new System.Windows.Forms.ComboBox();
+            this.grbDatosOficina.SuspendLayout();
+            this.grbDatosEmpleado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPisoOficina)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPiso)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grbDatosOficina
+            // 
+            this.grbDatosOficina.Controls.Add(this.dtpFechaIngreso);
+            this.grbDatosOficina.Controls.Add(this.txtDocumentoJefe);
+            this.grbDatosOficina.Controls.Add(this.txtApellidoJefe);
+            this.grbDatosOficina.Controls.Add(this.txtNombreJefe);
+            this.grbDatosOficina.Controls.Add(this.cmbDepartamentoOficina);
+            this.grbDatosOficina.Controls.Add(this.nudPisoOficina);
+            this.grbDatosOficina.Controls.Add(this.btnMostrar);
+            this.grbDatosOficina.Controls.Add(this.btnCrearOficina);
+            this.grbDatosOficina.Controls.Add(this.lblDni);
+            this.grbDatosOficina.Controls.Add(this.Ingreso);
+            this.grbDatosOficina.Controls.Add(this.lblApellidoJefe);
+            this.grbDatosOficina.Controls.Add(this.lblNombreJefe);
+            this.grbDatosOficina.Controls.Add(this.lblDepartamentoOficina);
+            this.grbDatosOficina.Controls.Add(this.lblPisoOficina);
+            this.grbDatosOficina.Location = new System.Drawing.Point(13, 13);
+            this.grbDatosOficina.Name = "grbDatosOficina";
+            this.grbDatosOficina.Size = new System.Drawing.Size(318, 265);
+            this.grbDatosOficina.TabIndex = 0;
+            this.grbDatosOficina.TabStop = false;
+            this.grbDatosOficina.Text = "Datos Oficina";
+            // 
+            // lblPisoOficina
+            // 
+            this.lblPisoOficina.AutoSize = true;
+            this.lblPisoOficina.Location = new System.Drawing.Point(16, 29);
+            this.lblPisoOficina.Name = "lblPisoOficina";
+            this.lblPisoOficina.Size = new System.Drawing.Size(27, 13);
+            this.lblPisoOficina.TabIndex = 0;
+            this.lblPisoOficina.Text = "Piso";
+            // 
+            // lblDepartamentoOficina
+            // 
+            this.lblDepartamentoOficina.AutoSize = true;
+            this.lblDepartamentoOficina.Location = new System.Drawing.Point(16, 56);
+            this.lblDepartamentoOficina.Name = "lblDepartamentoOficina";
+            this.lblDepartamentoOficina.Size = new System.Drawing.Size(74, 13);
+            this.lblDepartamentoOficina.TabIndex = 1;
+            this.lblDepartamentoOficina.Text = "Departamento";
+            // 
+            // lblNombreJefe
+            // 
+            this.lblNombreJefe.AutoSize = true;
+            this.lblNombreJefe.Location = new System.Drawing.Point(16, 88);
+            this.lblNombreJefe.Name = "lblNombreJefe";
+            this.lblNombreJefe.Size = new System.Drawing.Size(44, 13);
+            this.lblNombreJefe.TabIndex = 2;
+            this.lblNombreJefe.Text = "Nombre";
+            // 
+            // lblApellidoJefe
+            // 
+            this.lblApellidoJefe.AutoSize = true;
+            this.lblApellidoJefe.Location = new System.Drawing.Point(16, 118);
+            this.lblApellidoJefe.Name = "lblApellidoJefe";
+            this.lblApellidoJefe.Size = new System.Drawing.Size(44, 13);
+            this.lblApellidoJefe.TabIndex = 3;
+            this.lblApellidoJefe.Text = "Apellido";
+            // 
+            // Ingreso
+            // 
+            this.Ingreso.AutoSize = true;
+            this.Ingreso.Location = new System.Drawing.Point(19, 175);
+            this.Ingreso.Name = "Ingreso";
+            this.Ingreso.Size = new System.Drawing.Size(52, 13);
+            this.Ingreso.TabIndex = 5;
+            this.Ingreso.Text = "lblIngreso";
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(19, 145);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(26, 13);
+            this.lblDni.TabIndex = 6;
+            this.lblDni.Text = "DNI";
+            // 
+            // btnCrearOficina
+            // 
+            this.btnCrearOficina.Location = new System.Drawing.Point(19, 215);
+            this.btnCrearOficina.Name = "btnCrearOficina";
+            this.btnCrearOficina.Size = new System.Drawing.Size(90, 32);
+            this.btnCrearOficina.TabIndex = 7;
+            this.btnCrearOficina.Text = "Crear Oficina";
+            this.btnCrearOficina.UseVisualStyleBackColor = true;
+            this.btnCrearOficina.Click += new System.EventHandler(this.btnCrearOficina_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(183, 215);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(82, 32);
+            this.btnMostrar.TabIndex = 8;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // grbDatosEmpleado
+            // 
+            this.grbDatosEmpleado.Controls.Add(this.cmbDepartamento);
+            this.grbDatosEmpleado.Controls.Add(this.nudPiso);
+            this.grbDatosEmpleado.Controls.Add(this.txtLegajo);
+            this.grbDatosEmpleado.Controls.Add(this.txtApellido);
+            this.grbDatosEmpleado.Controls.Add(this.txtNombre);
+            this.grbDatosEmpleado.Controls.Add(this.lblDepartamento);
+            this.grbDatosEmpleado.Controls.Add(this.lblPiso);
+            this.grbDatosEmpleado.Controls.Add(this.lblLegajo);
+            this.grbDatosEmpleado.Controls.Add(this.lblApellido);
+            this.grbDatosEmpleado.Controls.Add(this.lblNombre);
+            this.grbDatosEmpleado.Controls.Add(this.btnAgregar);
+            this.grbDatosEmpleado.Location = new System.Drawing.Point(366, 13);
+            this.grbDatosEmpleado.Name = "grbDatosEmpleado";
+            this.grbDatosEmpleado.Size = new System.Drawing.Size(314, 265);
+            this.grbDatosEmpleado.TabIndex = 1;
+            this.grbDatosEmpleado.TabStop = false;
+            this.grbDatosEmpleado.Text = "Datos Empleado";
+            // 
+            // nudPisoOficina
+            // 
+            this.nudPisoOficina.Location = new System.Drawing.Point(108, 29);
+            this.nudPisoOficina.Name = "nudPisoOficina";
+            this.nudPisoOficina.Size = new System.Drawing.Size(120, 20);
+            this.nudPisoOficina.TabIndex = 9;
+            // 
+            // cmbDepartamentoOficina
+            // 
+            this.cmbDepartamentoOficina.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDepartamentoOficina.FormattingEnabled = true;
+            this.cmbDepartamentoOficina.Location = new System.Drawing.Point(108, 56);
+            this.cmbDepartamentoOficina.Name = "cmbDepartamentoOficina";
+            this.cmbDepartamentoOficina.Size = new System.Drawing.Size(121, 21);
+            this.cmbDepartamentoOficina.TabIndex = 10;
+            // 
+            // txtNombreJefe
+            // 
+            this.txtNombreJefe.Location = new System.Drawing.Point(108, 88);
+            this.txtNombreJefe.Name = "txtNombreJefe";
+            this.txtNombreJefe.Size = new System.Drawing.Size(157, 20);
+            this.txtNombreJefe.TabIndex = 11;
+            // 
+            // txtApellidoJefe
+            // 
+            this.txtApellidoJefe.Location = new System.Drawing.Point(108, 118);
+            this.txtApellidoJefe.Name = "txtApellidoJefe";
+            this.txtApellidoJefe.Size = new System.Drawing.Size(157, 20);
+            this.txtApellidoJefe.TabIndex = 12;
+            // 
+            // txtDocumentoJefe
+            // 
+            this.txtDocumentoJefe.Location = new System.Drawing.Point(108, 145);
+            this.txtDocumentoJefe.Name = "txtDocumentoJefe";
+            this.txtDocumentoJefe.Size = new System.Drawing.Size(157, 20);
+            this.txtDocumentoJefe.TabIndex = 13;
+            // 
+            // dtpFechaIngreso
+            // 
+            this.dtpFechaIngreso.Location = new System.Drawing.Point(108, 175);
+            this.dtpFechaIngreso.Name = "dtpFechaIngreso";
+            this.dtpFechaIngreso.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaIngreso.TabIndex = 14;
+            // 
+            // rtbDatos
+            // 
+            this.rtbDatos.Enabled = false;
+            this.rtbDatos.Location = new System.Drawing.Point(13, 285);
+            this.rtbDatos.Name = "rtbDatos";
+            this.rtbDatos.Size = new System.Drawing.Size(667, 209);
+            this.rtbDatos.TabIndex = 2;
+            this.rtbDatos.Text = "";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(26, 29);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(26, 56);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(44, 13);
+            this.lblApellido.TabIndex = 0;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblLegajo
+            // 
+            this.lblLegajo.AutoSize = true;
+            this.lblLegajo.Location = new System.Drawing.Point(26, 88);
+            this.lblLegajo.Name = "lblLegajo";
+            this.lblLegajo.Size = new System.Drawing.Size(39, 13);
+            this.lblLegajo.TabIndex = 0;
+            this.lblLegajo.Text = "Legajo";
+            // 
+            // lblPiso
+            // 
+            this.lblPiso.AutoSize = true;
+            this.lblPiso.Location = new System.Drawing.Point(26, 125);
+            this.lblPiso.Name = "lblPiso";
+            this.lblPiso.Size = new System.Drawing.Size(27, 13);
+            this.lblPiso.TabIndex = 0;
+            this.lblPiso.Text = "Piso";
+            // 
+            // lblDepartamento
+            // 
+            this.lblDepartamento.AutoSize = true;
+            this.lblDepartamento.Location = new System.Drawing.Point(26, 164);
+            this.lblDepartamento.Name = "lblDepartamento";
+            this.lblDepartamento.Size = new System.Drawing.Size(74, 13);
+            this.lblDepartamento.TabIndex = 0;
+            this.lblDepartamento.Text = "Departamento";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(215, 215);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(82, 32);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(115, 29);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(182, 20);
+            this.txtNombre.TabIndex = 9;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(115, 57);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(182, 20);
+            this.txtApellido.TabIndex = 10;
+            // 
+            // txtLegajo
+            // 
+            this.txtLegajo.Location = new System.Drawing.Point(115, 87);
+            this.txtLegajo.Name = "txtLegajo";
+            this.txtLegajo.Size = new System.Drawing.Size(182, 20);
+            this.txtLegajo.TabIndex = 11;
+            // 
+            // nudPiso
+            // 
+            this.nudPiso.Location = new System.Drawing.Point(115, 119);
+            this.nudPiso.Name = "nudPiso";
+            this.nudPiso.Size = new System.Drawing.Size(120, 20);
+            this.nudPiso.TabIndex = 12;
+            // 
+            // cmbDepartamento
+            // 
+            this.cmbDepartamento.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDepartamento.FormattingEnabled = true;
+            this.cmbDepartamento.Location = new System.Drawing.Point(115, 161);
+            this.cmbDepartamento.Name = "cmbDepartamento";
+            this.cmbDepartamento.Size = new System.Drawing.Size(121, 21);
+            this.cmbDepartamento.TabIndex = 13;
+            // 
+            // VistaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 506);
+            this.Controls.Add(this.rtbDatos);
+            this.Controls.Add(this.grbDatosEmpleado);
+            this.Controls.Add(this.grbDatosOficina);
+            this.Name = "VistaForm";
+            this.Text = "Vista del Oficina";
+            this.Load += new System.EventHandler(this.VistaForm_Load);
+            this.grbDatosOficina.ResumeLayout(false);
+            this.grbDatosOficina.PerformLayout();
+            this.grbDatosEmpleado.ResumeLayout(false);
+            this.grbDatosEmpleado.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPisoOficina)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPiso)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbDatosOficina;
+        private System.Windows.Forms.Label lblDepartamentoOficina;
+        private System.Windows.Forms.Label lblPisoOficina;
+        private System.Windows.Forms.Label Ingreso;
+        private System.Windows.Forms.Label lblApellidoJefe;
+        private System.Windows.Forms.Label lblNombreJefe;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnCrearOficina;
+        private System.Windows.Forms.GroupBox grbDatosEmpleado;
+        private System.Windows.Forms.DateTimePicker dtpFechaIngreso;
+        private System.Windows.Forms.TextBox txtDocumentoJefe;
+        private System.Windows.Forms.TextBox txtApellidoJefe;
+        private System.Windows.Forms.TextBox txtNombreJefe;
+        private System.Windows.Forms.ComboBox cmbDepartamentoOficina;
+        private System.Windows.Forms.NumericUpDown nudPisoOficina;
+        private System.Windows.Forms.Label lblDepartamento;
+        private System.Windows.Forms.Label lblPiso;
+        private System.Windows.Forms.Label lblLegajo;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.RichTextBox rtbDatos;
+        private System.Windows.Forms.ComboBox cmbDepartamento;
+        private System.Windows.Forms.NumericUpDown nudPiso;
+        private System.Windows.Forms.TextBox txtLegajo;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
---
+++ parcial1/VistaForm/VistaForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace VistaForm
+{
+    public partial class VistaForm : Form
+    {
+        public VistaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VistaForm_Load(object sender, EventArgs e)
+        {
+            cmbDepartamento.DataSource = Enum.GetValues(typeof(Entidades.Departamentos));
+            cmbDepartamentoOficina.DataSource = Enum.GetValues(typeof(Entidades.Departamentos));
+        }
+
+        private void btnCrearOficina_Click(object sender, EventArgs e)
+        {
+            //Oficina oficina;
+            //short piso; 
+            //    short.TryParse(nudPisoOficina.ToString(),out piso);
+            //oficina = new Oficina(
+        }
+
+        
+
+        
+
+        
+    }
+}
