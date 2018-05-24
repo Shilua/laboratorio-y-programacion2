8222b47009e194f16ccd32adaf0e384e1509cd10
alumno <alumno>
2018-05-23 22:13:40 -0300

    generics 48 terminado

---
+++ Generics/Generics48/Documento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics48
+{
+    public class Documento
+    {
+        #region fields
+        private int numero;
+        #endregion
+        public int Numero
+        {
+            get
+            { return this.numero; }
+        }
+
+
+        public Documento(int numero)
+        {
+            this.numero = numero;
+        }
+    }
+}
---
+++ Generics/contador/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics48;
+
+namespace contador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recibo res = new Recibo(20);
+            Recibo res2 = new Recibo(30);
+            Factura fac = new Factura(11);
+            Factura fac2 = new Factura(27);
+
+            Contabilidad<Factura,Recibo> contador = new Contabilidad<Factura,Recibo>();
+            contador += res;
+            contador += res2;
+            contador += fac;
+            contador += fac2;
+
+            Console.WriteLine(contador.Mostrar());
+            Console.ReadKey();
+            
+        }
+    }
+}
