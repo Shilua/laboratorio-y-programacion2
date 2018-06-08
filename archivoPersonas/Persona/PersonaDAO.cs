b0d81543228df6ad599db9d6eb8f42df3a9eb2c2
unknown <shilua4@gmail.com>
2018-06-08 14:35:32 -0300

    viernes

--- archivoPersonas/Persona/PersonaDAO.cs
+++ archivoPersonas/Persona/PersonaDAO.cs
@@ -22,5 +22,40 @@ namespace Persona
             PersonaDAO.comando.CommandType = System.Data.CommandType.Text;
             PersonaDAO.comando.Connection = PersonaDAO.conexion;
         }
+
+        public static List<Persona> Leer()
+        {
+            bool todoOk = false;
+            List<Persona> personas = new List<Persona>();
+            try
+            {
+                PersonaDAO.comando.CommandText = "SELECT * FROM Persona";
+                PersonaDAO.conexion.Open();
+                SqlDataReader oDr = PersonaDAO.comando.ExecuteReader();
+                while(oDr.Read())
+                {
+                    Persona p1 = new Persona(int.Parse(oDr["ID"].ToString()), oDr["Nombre"].ToString(), oDr["Apellido"].ToString());
+                    personas.Add(p1);
+                }
+                oDr.Close();
+                todoOk = true;
+            }
+
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if(todoOk)
+                {
+                    PersonaDAO.conexion.Close();
+                }
+            }
+            
+            return personas;               
+        }
+
+
     }
 }
