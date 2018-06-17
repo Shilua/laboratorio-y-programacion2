88ecdda825a1071483599bae417c90a128e9a392
unknown <shilua4@gmail.com>
2018-06-17 18:30:26 -0300

    tp04

---
+++ tp 04/CorreoLibrary/Correo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Correo /*: IMostrar<T>*/
+    {
+        //public string MostrarDatos(IMostrar<T> elemento)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ tp 04/CorreoLibrary/Paquete.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Paquete :  IMostrar<T>
+    {
+
+        #region Field
+        private string direccionEntrega;
+        private EEstado estado;
+        private string trackingID;
+        #endregion
+
+        #region Properties
+
+        public string DireccionEntrega
+        {
+            get
+            {
+                return this.direccionEntrega;
+            }
+            set
+            {
+                this.direccionEntrega = value;
+            }
+        }
+
+        public EEstado Estado
+        {
+            get
+            {
+                return this.estado;
+            }
+            set
+            {
+                this.estado = value;
+            }
+        }
+
+        public string TrackingID
+        {
+            get
+            {
+                return this.trackingID;
+            }
+            set
+            {
+                this.trackingID = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Nested Types
+        public enum EEstado
+        {
+
+        }
+        #endregion
+
+    }
+}
